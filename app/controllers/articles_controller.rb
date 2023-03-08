89e05458550048e1dc502e23565e4a286673eb3c
oliverbill <alves.bill@gmail.com>
2023-03-08 14:31:54 -0300

    initial version

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @articles = Article.all
+    @article = Article.find(params[:id])
+  end
 end
---
+++ db/migrate/20230306144914_add_column_author_to_comments.rb
@@ -0,0 +1,5 @@
+class AddColumnAuthorToComments < ActiveRecord::Migration[6.1]
+  def change
+    add_column :comments, :author, :string
+  end
+end
